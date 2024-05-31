dae5326889112c5c4af74f03ce2415c910a1a13f
Anna Belousova <belusovanna@gmail.com>
2024-05-31 17:53:07 +0500

    added getMessage() method

--- src/main/java/com/gmail/belusovanna/Main.java
+++ src/main/java/com/gmail/belusovanna/Main.java
@@ -2,6 +2,10 @@ package com.gmail.belusovanna;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "Hello qa.guru!";
     }
 }
