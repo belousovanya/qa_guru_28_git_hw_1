3ecc156ce9b7b85e05d6db0ade1c03bf8e3d3d10
Anna Belousova <belusovanna@gmail.com>
2024-05-31 19:16:29 +0500

    added Hello qa.guru again

--- src/main/java/com/gmail/belusovanna/Main.java
+++ src/main/java/com/gmail/belusovanna/Main.java
@@ -1,7 +1,7 @@
 package com.gmail.belusovanna;
 
 public class Main {
-    private static String msg = "Hello qa.guru!";
+    private static String msg = "Hello qa.guru!!";
 
     public static void main(String[] args) {
         System.out.println(getMessage());
