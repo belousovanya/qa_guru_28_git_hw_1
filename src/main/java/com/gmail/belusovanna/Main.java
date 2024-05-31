a80e9b1e44b6f9c35357b339df5ce2da505eb7c7
Anna Belousova <belusovanna@gmail.com>
2024-05-31 18:33:50 +0500

    Update Main.java

--- src/main/java/com/gmail/belusovanna/Main.java
+++ src/main/java/com/gmail/belusovanna/Main.java
@@ -3,7 +3,7 @@ package com.gmail.belusovanna;
 public class Main {
     private static String msg = "Hello qa.guru!"
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(msg);
     }
 }
